f370db81c1d68ccd14f461e1ebdf888f96f7d81c
PatrickMohr <61147810+PatrickMohr@users.noreply.github.com>
2024-02-27 16:45:32 +0100

    Personal projects frontpage

---
+++ pages/cookingcompanion.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="title-container">
+    <h1>CookingCompanion</h1>
+  </div>
+  <div class="container">
+    <div class="title-container">
+      <h1>Coming soon..</h1>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+.title-container {
+  text-align: center;
+  font-family: 'Arial', sans-serif;
+  margin: 2vw;
+  font-size: 1.2vw;
+  color: #363636;
+}
+
+</style>
